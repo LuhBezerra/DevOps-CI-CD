3bce530e51dbddaf64fc54714f936cbd766cc3b8
Samuel <samuelveloso.contato@gmail.com>
2022-08-25 20:16:23 -0300

    Initial commit

---
+++ src/test/java/com/ifal/springbootdevops/SpringBootDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifal.springbootdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
