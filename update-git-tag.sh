cb26446ab5d8b22d687aa38efe01cbc9cf512def
Jonata Ramalho <jonataramalho.s@gmail.com>
2022-11-21 21:54:33 -0300

    chore: Update script

--- update-git-tag.sh
+++ update-git-tag.sh
@@ -3,6 +3,9 @@
 CURRENT_VERSION=$(eval "git describe --abbrev=0 --tags")
 
 UPDATED_VERSION=$((CURRENT_VERSION+1))
+
+git remote set-url origin https://gitlab.com/samuelveloso/spingbootdevops.git
+
 git tag ${VERSION}
 git push --tags
 
